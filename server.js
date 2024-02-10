b32f7d7ab1f6d8eec0d8a4c62dae9dbef282a36d
Pulkit Goyal <54586310+coolpulkit@users.noreply.github.com>
2024-02-10 20:48:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require('./app');
-const config = require('./utils/config');
-const logger = require('./utils/logger');
+// const config = require('./utils/config');
+// const logger = require('./utils/logger');
 
-const PORT = config.PORT || 3001;
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
-});
+// const PORT = config.PORT || 3001;
+// app.listen(PORT, () => {
+//   logger.info(`Server running on port ${config.PORT}`);
+// });
+module.exports = async (req, res) => {
+  // If you need to do any custom logic based on the request before passing it to Express,
+  // you can do it here
+  
+  // Pass the request to the Express app
+  await app(req, res);
+};
