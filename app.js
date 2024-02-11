2a8a3fa4da13e2000513fc1b82d8ec414fb6b89b
Dell <vani1497.be21@chitkara.edu.in>
2024-02-11 22:05:04 +0530

    d

--- app.js
+++ app.js
@@ -25,13 +25,41 @@ app.use(cookieParser());
 app.use(middleware.requestLogger);
 
 // connect to db
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB');
-  })
-  .catch((err) => {
-    logger.error('error connecting to MongoDB:', err.message);
-  });
+// mongoose.connect(config.MONGODB_URI)
+//   .then(() => {
+//     logger.info('connected to MongoDB');
+//   })
+//   .catch((err) => {
+//     logger.error('error connecting to MongoDB:', err.message);
+//   });
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+//const uri = "mongodb+srv://vania1497be21:<password>@cluster0.vszjtji.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(config.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.use('/api/users', usersRouter);
 app.use('/api/links', linksRouter);
